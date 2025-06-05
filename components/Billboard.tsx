f964ab203d5130b6ff6a07ed676f889a501eb2a4
mahdi6788 <mjalalimech@gmail.com>
2025-06-05 22:38:24 +0330

    refactor: restructure Billboard component layout for improved readability and styling

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -2,17 +2,18 @@ import { BillboardType } from "@/lib/types";
 
 export default function Billboard({ data }: { data: BillboardType }) {
   return (
-    <div
-      className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden"
-      style={{ backgroundImage: `url(${data?.imageUrl})` }}
-    >
-      <div className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden">
-        <div className="flex flex-col items-center justify-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
-            {data.label}
-          </div>
+    <>
+      <div className="font-bold text-xl sm:text-2xl lg:text-3xl sm:max-w-xl max-w-xs">
+        {data.label}
+      </div>
+      <div
+        className="m-2 sm:m-4 lg:m-6 rounded-xl overflow-hidden"
+        style={{ backgroundImage: `url(${data?.imageUrl})` }}
+      >
+        <div className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden">
+          <div className="flex flex-col items-center justify-center text-center gap-y-8"></div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
